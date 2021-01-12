55d9d01ecc3abfa9f596fa5eaad5ca20481e97c6
Daniel Engvall <daniel@engvalls.eu>
2021-01-12 08:54:06 +0100

    Fixes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from exchangelib import Credentials, Account
 import os
 from peewee import *
+from collections import namedtuple as nt
 
 db = SqliteDatabase('emails.sqlite')
 
@@ -20,29 +21,24 @@ class Mail(Model):
 def collect_mail(email, password):
     credentials = Credentials(email, password)
     account = Account(email, credentials=credentials, autodiscover=True)
+    fields = ('datetime', 'sender', 'to', 'cc', 'subject', 'body')
 
     for item in account.inbox.all().order_by('-datetime_received')[:3]:
-        datetime = item.datetime_received.strftime('%Y-%m-%d %H:%M:%S')
-        body = item.body
-        subject = item.subject
-        to = ','.join([f'{_.name} <{_.email_address}>' for _ in item.to_recipients])
-        sender = f'{item.sender.name} <{item.sender.email_address}>'
-        cc = str(item.display_cc)
-
-        fields = ('datetime', 'sender', 'to', 'cc', 'subject', 'body')
-
-        for _ in fields:
-            print(f'{_}: {locals().get(_)}')
-            result = (Mail
-                      .insert({_: locals().get(_) for _ in fields})
-                      .on_conflict('replace')
-                      .execute())
-        print('-'*30)
+        f = nt('f', fields)(str(item.datetime_received.strftime('%Y-%m-%d %H:%M:%S')),
+                            f'{item.sender.name} <{item.sender.email_address}>',
+                            ','.join([f'{_.name} <{_.email_address}>' for _ in item.to_recipients]),
+                            str(item.display_cc),
+                            item.subject,
+                            item.body)
+        print(f)
+        result = (Mail
+                  .insert(**f._asdict())
+                  .on_conflict('replace')
+                  .execute())
 
 
 if __name__ == '__main__':
     db.create_tables([Mail, ])
-
     email = os.environ.get('EMAIL')
     password = os.environ.get('PASSWORD')
     collect_mail(email, password)
