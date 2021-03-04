ea22dcfbd41978b1b440a8155edbe1301a7266b4
Daniel Engvall <daniel@engvalls.eu>
2021-03-04 21:04:41 +0100

    Refactoring, adding setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='mail_export',
+    version='0.0.1',
+    packages=['richlog', 'exchange', 'mail_exporter'],
+    install_requires=open('requirements.txt').read().split('\n'),
+    url='',
+    license='MIT',
+    author='Daniel Engvall',
+    author_email='daniel@engvalls.eu',
+    description='An application for exporting existing emails',
+    entry_points={
+        'console_scripts': ['mail_export=mail_export.__main__:main']
+    }
+)
